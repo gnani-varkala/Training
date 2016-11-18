2dbfdb710c0b1d30ff91bcf117da55d42b1b3e51
gnani-varkala <varkala.gnani@gmail.com>
2016-11-18 22:14:32 +0530

    carousel

---
+++ carousel/carousel/JS/modalScript.js
@@ -0,0 +1,18 @@
+
+
+$(document).ready(function(){
+	$(".container").click(function() {
+		var editid = $(this).find("iframe");
+		var url = $(editid).attr('src')+"?autoplay=1";
+		var myId = $($(this).find("button")).attr('data-target');
+		$(myId).on('hide.bs.modal', function(e){
+			$(".cartoonVideo").attr('src', '');
+		});
+
+		$(myId).on('show.bs.modal', function(e){
+			$(".cartoonVideo").attr('src', url);
+		});
+	});
+
+
+});
