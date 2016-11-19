a99bb8b212fda3c1cf145ef87f5d52b40ca2ee20
gnani-varkala <varkala.gnani@gmail.com>
2016-11-19 10:48:35 +0530

    json to table

---
+++ json_table.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+	 /*var Employees= [
+	{
+      "ID": "30024",
+      "Name": "NagaLatha",
+      "Designation": "Trainee",
+      "Experience": "5Days",
+      "Salary": 20000
+    },
+    {
+      "ID": "30025",
+      "Name": "Santoshi",
+      "Designation": "Trainee",
+      "Experience": "5Days",
+      "Salary": 20000
+    }
+  ];*/
+		var Employees = [];
+		$.ajax({url: "JSON/details.json", success: function(result){
+			Employees = result;
+		
+		$("#test").html($("<tbody></tbody>"))
+		var table = $("#test").children()[0];
+		$(table).append($("<tr></tr>"));
+		var header = $(table).children()[0];
+			for(var index in Employees[0]){
+				$(header).append("<th>"+index+"</th>");
+			}
+			//appending array elements to DOM
+			for(var i=0;i<Employees.length;i++){
+				var tr = $("<tr></tr>")
+				for(var index in Employees[i]){
+					$(tr).append("<td>"+Employees[i][index]+"</td>");
+				}
+					$(table).append(tr);
+			}
+		 }});
+});
